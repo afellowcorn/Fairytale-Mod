0b1b138cedb2efc68078d371070445276db7888f
Acorn <a.fellow.corn@gmail.com>
2024-03-23 11:55:30 +0100

    New names + no error for existing saves

--- scripts/cat/names.py
+++ scripts/cat/names.py
@@ -178,8 +178,13 @@ class Name():
                 self.suffix = random.choice(self.names_dict["normal_suffixes"])
 
     def __repr__(self):
+        if not self.prefix in self.names_dict['all_prefixes']:
+            if not self.suffix:
+                return self.prefix
+            return self.prefix + ' ' + self.suffix
+            
         # Handles predefined suffixes (such as newborns being kit), then suffixes based on ages (fixes #2004, just trust me)
-        if self.status in ['newborn', 'kitten']:
+        elif self.status in ['newborn', 'kitten']:
             if self.gender == 'female' and 'dimf' in self.names_dict['all_prefixes'][self.prefix]:
                 if ('dimaf' in self.names_dict['all_prefixes'][self.prefix]) and (self.eyes in ['YELLOW', 'AMBER', 'HAZEL', 'PALEGREEN', 'GREEN', 'BLUE', 'DARKBLUE', 'GREY', 'CYAN', 'EMERALD', 'PALEBLUE']):
                     return self.names_dict['all_prefixes'][self.prefix]['dimaf']
